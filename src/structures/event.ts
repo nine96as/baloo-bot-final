f354d4d76816da967724d72eab8ae64078e233f3
996as <ashrafsaid79@gmail.com>
2023-02-19 15:08:13 +0000

    feat: added PlayerEvent interface

--- src/structures/event.ts
+++ src/structures/event.ts
@@ -1,8 +1,14 @@
 import { Bot } from './bot.js';
 import { ClientEvents } from 'discord.js';
+import { PlayerEvents } from 'discord-music-player';
 
 export interface Event {
   name: keyof ClientEvents;
   once?: boolean;
+  execute(client: Bot, ...args: unknown[]): void | Promise<void>;
+}
+
+export interface PlayerEvent {
+  name: keyof PlayerEvents;
   execute(client: Bot, ...args: unknown[]): void | Promise<void>;
 }
