9b35535eae99b77c16f998c8dd44495ac7533a18
996as <nine96@skiff.com>
2023-02-19 15:06:32 +0000

    feat: created channelEmpty player event

---
+++ src/events/player/channelEmpty.ts
@@ -0,0 +1,20 @@
+import { GuildTextBasedChannel } from 'discord.js';
+import { Queue } from 'discord-music-player';
+import { Bot, InfoEmbed, PlayerEvent } from '#structures';
+
+export const event: PlayerEvent = {
+  name: 'channelEmpty',
+  async execute(client: Bot, queue: Queue) {
+    const data = queue.data as any;
+    const channelId = data.channelId;
+    const channel = client.channels.cache.get(
+      channelId
+    ) as GuildTextBasedChannel;
+
+    await channel.send({
+      embeds: [
+        new InfoEmbed(`***${channel.name} is empty, leaving the channel***`)
+      ]
+    });
+  }
+};
