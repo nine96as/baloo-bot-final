1d9a92a180564879aa279c55319a71542cdfdde4
996as <nine96@skiff.com>
2023-02-19 15:06:45 +0000

    feat: created "error" player event

---
+++ src/events/player/error.ts
@@ -0,0 +1,20 @@
+import { GuildTextBasedChannel } from 'discord.js';
+import { DMPError, Queue } from 'discord-music-player';
+import { Bot, ErrorEmbed, PlayerEvent } from '#structures';
+import { logger } from '#functions';
+
+export const event: PlayerEvent = {
+  name: 'error',
+  async execute(client: Bot, error: DMPError, queue: Queue) {
+    const data = queue.data as any;
+    const channelId = data.channelId;
+    const channel = client.channels.cache.get(
+      channelId
+    ) as GuildTextBasedChannel;
+
+    logger.error(`${error} in ${queue.guild.name}`);
+    await channel.send({
+      embeds: [new ErrorEmbed('***playerError***')]
+    });
+  }
+};
