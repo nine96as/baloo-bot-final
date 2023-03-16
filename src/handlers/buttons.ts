1303e6c75b5a08689c13aa61bc253c4ce535076e
996as <ashrafsaid79@gmail.com>
2023-03-16 23:08:09 +0000

    tweak: commenting

--- src/handlers/buttons.ts
+++ src/handlers/buttons.ts
@@ -1,16 +1,28 @@
-import { Bot } from '#structures';
+import { Bot, Button } from '#structures';
 import { getContents, logger } from '#functions';
 import { fileURLToPath } from 'url';
 
-export async function loadButtons(client: Bot) {
+/**
+ * Loads all buttons from the 'buttons' directory and adds them to the client's 'buttons' collection.
+ * @param {Bot} client - The Discord client instance.
+ * @returns {Promise<void>} - A Promise that resolves when all buttons have been loaded.
+ */
+export async function loadButtons(client: Bot): Promise<void> {
+  // Get the absolute path to the 'buttons' directory.
   const dirname = fileURLToPath(new URL('../buttons', import.meta.url));
+  // Get an array of all button files in the 'buttons' directory and its subdirectories.
   const contents = await getContents(dirname);
 
   for (const content of contents) {
-    const { button } = content;
+    // Extract the button object from the content object.
+    const { button } = content as Button;
     if (button === undefined || button.customId === undefined) {
-      logger.error(`error exporting button.`);
+      logger.error(
+        `error exporting button, succeeded in loading ${client.buttons.size} button(s).`
+      );
+      process.exit(1);
     } else {
+      // Add the button object to the client's 'buttons' collection, with its customId as the key.
       client.buttons.set(button.customId, button);
     }
   }
