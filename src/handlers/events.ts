efde3ca57efa4852b2635eb6742a3b4178d5c86a
996as <ashrafsaid79@gmail.com>
2023-03-16 23:08:27 +0000

    tweak: commenting, removed player event

--- src/handlers/events.ts
+++ src/handlers/events.ts
@@ -1,4 +1,4 @@
-import { Bot } from '#structures';
+import { Bot, Event } from '#structures';
 import { getContents, logger } from '#functions';
 import { fileURLToPath } from 'url';
 
@@ -7,7 +7,7 @@ export async function loadEvents(client: Bot) {
   const contents = await getContents(dirname);
 
   for (const content of contents) {
-    const { event } = content;
+    const { event } = content as Event;
     if (event.once) {
       client.once(event.name, (...args: unknown[]) =>
         event.execute(client, ...args)
@@ -20,24 +20,4 @@ export async function loadEvents(client: Bot) {
   }
 
   logger.info(`loaded client events.`);
-}
-
-export async function loadPlayerEvents(client: Bot) {
-  const dirname = fileURLToPath(new URL('../events/player', import.meta.url));
-  const contents = await getContents(dirname);
-
-  for (const content of contents) {
-    const { event } = content;
-    if (event.once) {
-      client.player.once(event.name, (...args: unknown[]) =>
-        event.execute(client, ...args)
-      );
-    } else {
-      client.player.on(event.name, (...args: unknown[]) =>
-        event.execute(client, ...args)
-      );
-    }
-  }
-
-  logger.info(`loaded player events.`);
 }
