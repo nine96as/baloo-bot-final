8296a13adf8d8344b2212cc35e32ef961218625e
996as <nine96@skiff.com>
2023-03-16 23:09:56 +0000

    tweak: commenting, removed player event

--- src/structures/event.ts
+++ src/structures/event.ts
@@ -1,14 +1,26 @@
 import { Bot } from './bot.js';
 import { ClientEvents } from 'discord.js';
-import { PlayerEvents } from 'discord-music-player';
 
+/**
+ * Represents a Discord event to be handled by the bot.
+ */
 export interface Event {
+  /**
+   * A property that defines the name of the event to handle.
+   */
   name: keyof ClientEvents;
-  once?: boolean;
-  execute(client: Bot, ...args: unknown[]): void | Promise<void>;
-}
 
-export interface PlayerEvent {
-  name: keyof PlayerEvents;
+  /**
+   * An optional boolean property that indicates whether the event should only be handled once
+   * (i.e., not for subsequent occurrences of the event).
+   */
+  once?: boolean;
+
+  /**
+   * Executes the logic for the event.
+   * @param client - The Discord bot client instance that received the event.
+   * @param args - The arguments passed to the event
+   * @returns A promise that resolves to void.
+   */
   execute(client: Bot, ...args: unknown[]): void | Promise<void>;
 }
