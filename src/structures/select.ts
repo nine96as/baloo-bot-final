d03dc2b21b72bf44671405a49ff1fb1f74edb502
996as <ashrafsaid79@gmail.com>
2023-03-16 23:11:22 +0000

    tweak: commenting

--- src/structures/select.ts
+++ src/structures/select.ts
@@ -1,14 +1,27 @@
 import { Bot } from './bot.js';
 import {
-  SelectMenuInteraction,
+  AnySelectMenuInteraction,
   InteractionResponse,
   Message
 } from 'discord.js';
 
+/**
+ * Represents a select menu that can be interacted with by a user in a Discord server.
+ */
 export interface SelectMenu {
+  /**
+   * A string that represents the custom ID of the select menu.
+   */
   customId: string;
+
+  /**
+   * Executes the logic for the select menu when a user submits a select menu form.
+   * @param interaction - The interaction object representing the interaction that triggered the select menu submission.
+   * @param client - The Discord bot client instance that received the event.
+   * @returns A promise that resolves to a `InteractionResponse<boolean>`, `Message<boolean>`, or void.
+   */
   execute(
-    interaction: SelectMenuInteraction,
+    interaction: AnySelectMenuInteraction,
     client: Bot
   ): Promise<InteractionResponse<boolean> | Message<boolean> | void>;
 }
