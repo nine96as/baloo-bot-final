d2413e5fd294d52d8cd60a87d3e1591b4248ba45
996as <ashrafsaid79@gmail.com>
2023-03-16 23:12:06 +0000

    tweak: fixed any assertion

--- src/functions/getContents.ts
+++ src/functions/getContents.ts
@@ -3,9 +3,9 @@ import glob from 'tiny-glob';
 /**
  * fetches all file contents in a path
  * @param {string} path path to search
- * @returns {Array} list of files in path
+ * @returns {Promise<Array<unknown>>} list of files in path
  */
-export async function getContents(path: string): Promise<any[]> {
+export async function getContents(path: string): Promise<Array<unknown>> {
   const files = await glob(`${path}/**/*.{ts,js}`);
 
   const contents = await Promise.all(
