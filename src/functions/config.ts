274938e16d8208190ecf84d41c72f871ea836cc8
996as <nine96@skiff.com>
2023-03-16 23:11:02 +0000

    tweak: commenting, storing of intents

--- src/functions/config.ts
+++ src/functions/config.ts
@@ -1,14 +1,33 @@
 import { logger } from '#functions';
+import { GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 const { token, clientId, developerGuildId } = process.env;
 
+// If env variables are missing, will output error
 if (!token || !clientId || !developerGuildId) {
   logger.error('missing environment variables.');
   process.exit(9);
 }
 
+/**
+ * Holds a record of .env variables
+ */
 export const config: Record<string, string> = {
   token,
   clientId,
   developerGuildId
 };
+
+/**
+ * Holds the client intents that the bot will use
+ * - Guilds: Allows bot to see guilds
+ * - GuildMembers: Allows bot to see guild members
+ * - GuildMessages: Allows bot to see guild messages
+ * - GuildVoiceStates: Allows bot to see guild voice states
+ */
+export const intents = [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMembers,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.GuildVoiceStates
+];
