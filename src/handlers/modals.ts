71a0ea04eb56175af86ca935eeba2c679b6345f1
996as <ashrafsaid79@gmail.com>
2023-03-16 23:09:02 +0000

    tweak: commenting

--- src/handlers/modals.ts
+++ src/handlers/modals.ts
@@ -1,16 +1,27 @@
-import { Bot } from '#structures';
+import { Bot, Modal } from '#structures';
 import { getContents, logger } from '#functions';
 import { fileURLToPath } from 'url';
 
+/**
+ * Loads all modals from the 'modals' directory and adds them to the client's 'modals' collection.
+ * @param {Bot} client - The Discord client instance.
+ * @returns {Promise<void>} - A Promise that resolves when all modals have been loaded.
+ */
 export async function loadModals(client: Bot) {
+  // Get the absolute path to the 'modals' directory.
   const dirname = fileURLToPath(new URL('../modals', import.meta.url));
+  // Get an array of all modal files in the 'modals' directory and its subdirectories.
   const contents = await getContents(dirname);
 
   for (const content of contents) {
-    const { modal } = content;
+    // Extract the modal object from the content object.
+    const { modal } = content as Modal;
     if (modal === undefined || modal.customId === undefined) {
-      logger.error(`error exporting modal.`);
+      logger.error(
+        `error exporting modal, succeeded in loading ${client.modals.size} modal(s).`
+      );
     } else {
+      // Add the modal object to the client's 'modals' collection, with its customId as the key.
       client.modals.set(modal.customId, modal);
     }
   }
