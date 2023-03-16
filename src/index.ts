8129553b4692dbb16874da085c592e32f96e4117
996as <nine96@skiff.com>
2023-03-16 23:07:20 +0000

    tweak: commenting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { Bot } from '#structures';
 import { config } from '#functions';
 
+/**
+ * Creates a new instance of the Bot class and passes in a Discord bot token,
+ * which is retrieved from the config object using the token property.
+ */
 new Bot(config.token);
