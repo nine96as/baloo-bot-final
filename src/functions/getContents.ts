19212544413577a66746b2cb623935aa50cee6cb
996as <nine96@skiff.com>
2023-03-17 19:14:01 +0000

    tweak: commenting, revert to any typing

--- src/functions/getContents.ts
+++ src/functions/getContents.ts
@@ -1,11 +1,11 @@
 import glob from 'tiny-glob';
 
 /**
- * fetches all file contents in a path
- * @param {string} path path to search
- * @returns {Promise<Array<unknown>>} list of files in path
+ * Fetches all file contents in a path.
+ * @param {string} path Path to search.
+ * @returns {Promise<any[]>} List of files in path.
  */
-export async function getContents(path: string): Promise<Array<unknown>> {
+export async function getContents(path: string): Promise<any[]> {
   const files = await glob(`${path}/**/*.{ts,js}`);
 
   const contents = await Promise.all(
