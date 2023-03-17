f9ae9c9d30b8ad31d9051a35de42d29bf321a1a5
996as <nine96@skiff.com>
2023-03-17 19:14:53 +0000

    tweak: much better error checking, prod/dev command deploy

--- src/handlers/commands.ts
+++ src/handlers/commands.ts
@@ -4,9 +4,6 @@ import { config, getContents, logger } from '#functions';
 import { fileURLToPath } from 'url';
 const { clientId, developerGuildId, token } = config;
 
-const commandArray: JSON[] = [];
-const developerArray: JSON[] = [];
-
 /**
  * Loads all commands from the 'commands' directory and adds them to the client's 'commands' collection.
  * @param {Bot} client - The Discord client instance.
@@ -20,43 +17,46 @@ export async function loadCommands(client: Bot) {
 
   for (const content of contents) {
     // Extract the command object from the content object.
-    const { command } = content as Command;
+    const { command }: { command: Command } = content;
     if (command === undefined || command.data === undefined) {
       logger.error(
-        `error exporting commands, succeeded in loading ${client.commands.size} command(s).`
+        `error exporting commands, last export: ${
+          client.commands.last()?.data.name
+        }`
       );
       process.exit(1);
     } else {
       // Add the command object to the client's 'commands' collection, with its name as the key.
       client.commands.set(command.data.name, command);
-      // Add the command object to the `commandArray` or `developerArray`, depending on whether the `developer` boolean value was added.
-      if (command.developer) {
-        developerArray.push(command.data.toJSON());
-      } else {
-        commandArray.push(command.data.toJSON());
-      }
     }
   }
 
-  await deployCommands();
+  await deployCommands(client);
 
-  logger.info(`loaded ${client.commands.size} command(s).`);
+  logger.info(`✅ loaded ${client.commands.size} command(s).`);
 }
 
 /**
  * Uses the `REST` class from the `discord.js` module to deploy the commands to Discord.
  */
-async function deployCommands() {
+async function deployCommands(client: Bot) {
   const rest = new REST({ version: '10' }).setToken(token);
+  const body = client.commands.map((command) => command.data.toJSON());
 
-  await Promise.all([
-    // Loading of developer guild commands
-    rest.put(Routes.applicationGuildCommands(clientId, developerGuildId), {
-      body: developerArray
-    }),
-    // Loading of global commands
-    rest.put(Routes.applicationCommands(clientId), {
-      body: commandArray
-    })
-  ]);
+  // If the environment is production, commands will be registered globally.
+  const endpoint =
+    process.env.NODE_ENV === 'production'
+      ? Routes.applicationCommands(clientId)
+      : Routes.applicationGuildCommands(clientId, developerGuildId);
+
+  // Deploying commands to Discord.
+  try {
+    rest.put(endpoint, { body }).then(() => {
+      const response =
+        process.env.NODE_ENV === 'production' ? `✅ prod` : `✅ dev`;
+      logger.info(response);
+    });
+  } catch (e) {
+    logger.error(e);
+  }
 }
