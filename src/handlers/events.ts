41cf32f31ee1c90af4f7d590fef2d0e6d25a317e
996as <nine96@skiff.com>
2023-03-27 01:35:58 +0000

    tweak: added tabular logging

--- src/handlers/events.ts
+++ src/handlers/events.ts
@@ -1,23 +1,43 @@
 import { Bot, Event } from '#structures';
 import { getContents, logger } from '#functions';
 import { fileURLToPath } from 'url';
+import { table } from 'console';
 
-export async function loadEvents(client: Bot) {
+/**
+ * Loads all events from the 'events' directory and adds them to the client's 'events' collection.
+ * @param {Bot} client - The Discord client instance.
+ * @returns {Promise<void>} - A Promise that resolves when all events have been loaded.
+ */
+export const loadEvents = async (client: Bot): Promise<void> => {
+  // Get the absolute path to the 'events' directory.
   const dirname = fileURLToPath(new URL('../events/client', import.meta.url));
+  // Get an array of all event files in the 'commands' directory and its subdirectories.
   const contents = await getContents(dirname);
+  // Instantiates an array of all successfully loaded events.
+  const events = [];
 
   for (const content of contents) {
-    const { event }: { event: Event } = content;
-    if (event.once) {
-      client.once(event.name, (...args: unknown[]) =>
-        event.execute(client, ...args)
-      );
-    } else {
-      client.on(event.name, (...args: unknown[]) =>
-        event.execute(client, ...args)
+    try {
+      // Extract the button object from the content object.
+      const { event }: { event: Event } = content;
+
+      event.once
+        ? client.once(event.name, (...args: unknown[]) =>
+            event.execute(client, ...args)
+          )
+        : client.on(event.name, (...args: unknown[]) =>
+            event.execute(client, ...args)
+          );
+
+      // Add the command object to the client's 'commands' collection, with its name as the key.
+      client.events.set(event.name, event);
+      events.push({ event: event.name, status: '🟩' });
+    } catch (e) {
+      logger.error(
+        `error exporting events, last export: ${client.events.last()?.name}`
       );
     }
   }
 
-  logger.info(`✅ loaded client events.`);
-}
+  logger.info(table(events));
+};
