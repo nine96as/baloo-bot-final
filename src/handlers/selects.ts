f4f677f9938335da111322bff56481070a282e01
996as <nine96@skiff.com>
2023-03-27 01:36:11 +0000

    tweak: added tabular logging

--- src/handlers/selects.ts
+++ src/handlers/selects.ts
@@ -1,17 +1,20 @@
 import { Bot, SelectMenu } from '#structures';
 import { getContents, logger } from '#functions';
 import { fileURLToPath } from 'url';
+import { table } from 'console';
 
 /**
  * Loads all select menus from the 'selects' directory and adds them to the client's 'selects' collection.
  * @param {Bot} client - The Discord client instance.
  * @returns {Promise<void>} - A Promise that resolves when all select menus have been loaded.
  */
-export async function loadSelects(client: Bot) {
+export const loadSelects = async (client: Bot): Promise<void> => {
   // Get the absolute path to the 'selects' directory.
   const dirname = fileURLToPath(new URL('../selects', import.meta.url));
   // Get an array of all select menu files in the 'selects' directory and its subdirectories.
   const contents = await getContents(dirname);
+  // Instantiates an array of all successfully loaded selects.
+  const selects = [];
 
   for (const content of contents) {
     // Extract the select menu object from the content object.
@@ -25,9 +28,16 @@ export async function loadSelects(client: Bot) {
       process.exit(1);
     } else {
       // Add the select menu object to the client's 'selects' collection, with its customId as the key.
-      client.selects.set(select.customId, select);
+      try {
+        client.selects.set(select.customId, select);
+        selects.push({ select: select.customId, status: '🟩' });
+      } catch (e) {
+        `error exporting select menus, last export: ${
+          client.selects.last()?.customId
+        }`;
+      }
     }
   }
 
-  logger.info(`✅ loaded ${client.selects.size} select menu(s).`);
-}
+  logger.info(table(selects));
+};
