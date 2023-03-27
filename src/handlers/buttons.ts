49b041a5e0f3d2278fe50c28e45cb4d8a4303cd7
996as <ashrafsaid79@gmail.com>
2023-03-27 01:35:42 +0000

    tweak: added tabular logging

--- src/handlers/buttons.ts
+++ src/handlers/buttons.ts
@@ -1,17 +1,20 @@
 import { Bot, Button } from '#structures';
 import { getContents, logger } from '#functions';
 import { fileURLToPath } from 'url';
+import { table } from 'console';
 
 /**
  * Loads all buttons from the 'buttons' directory and adds them to the client's 'buttons' collection.
  * @param {Bot} client - The Discord client instance.
  * @returns {Promise<void>} - A Promise that resolves when all buttons have been loaded.
  */
-export async function loadButtons(client: Bot): Promise<void> {
+export const loadButtons = async (client: Bot): Promise<void> => {
   // Get the absolute path to the 'buttons' directory.
   const dirname = fileURLToPath(new URL('../buttons', import.meta.url));
   // Get an array of all button files in the 'buttons' directory and its subdirectories.
   const contents = await getContents(dirname);
+  // Instantiates an array of all successfully loaded buttons.
+  const buttons = [];
 
   for (const content of contents) {
     // Extract the button object from the content object.
@@ -25,9 +28,16 @@ export async function loadButtons(client: Bot): Promise<void> {
       process.exit(1);
     } else {
       // Add the button object to the client's 'buttons' collection, with its customId as the key.
-      client.buttons.set(button.customId, button);
+      try {
+        client.buttons.set(button.customId, button);
+        buttons.push({ button: button.customId, status: '🟩' });
+      } catch (e) {
+        `error exporting buttons, last export: ${
+          client.buttons.last()?.customId
+        }`;
+      }
     }
   }
 
-  logger.info(`loaded ${client.buttons.size} button(s).`);
-}
+  logger.info(table(buttons));
+};
