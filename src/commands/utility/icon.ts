5fe906d0154a617216acaeccfc516842735e0077
996as <nine96@skiff.com>
2023-03-27 21:22:17 +0000

    tweak: moved over cmd files

---
+++ src/commands/utility/icon.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { Command, Embed } from '#structures';
+
+export const command: Command = {
+  data: new SlashCommandBuilder()
+    .setName('icon')
+    .setDescription('🔬 get the server icon'),
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    if (interaction.inCachedGuild()) {
+      const { guild } = interaction;
+
+      return interaction.reply({
+        embeds: [
+          new Embed()
+            .setColor('Random')
+            .setAuthor({
+              name: guild.name,
+              iconURL: guild.iconURL() || undefined
+            })
+            .setImage(guild.iconURL({ size: 4096 }))
+        ]
+      });
+    }
+  }
+};
