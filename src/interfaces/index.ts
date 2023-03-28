40969f549f88ba64e69d1368a06f86eaf8c4fa99
996as <ashrafsaid79@gmail.com>
2023-03-28 00:05:47 +0000

    tweak: added #interface file refs

---
+++ src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export * from '../interfaces/event.js';
+export * from '../interfaces/command.js';
+export * from '../interfaces/button.js';
+export * from '../interfaces/select.js';
+export * from '../interfaces/modal.js';
