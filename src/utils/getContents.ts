db6118425247d1f1ef13726f867f0b6ac5d58afe
996as <nine96@skiff.com>
2023-03-28 01:31:35 +0000

    tweak: applied generics

--- src/utils/getContents.ts
+++ src/utils/getContents.ts
@@ -3,9 +3,9 @@ import glob from 'tiny-glob';
 /**
  * Fetches all file contents in a path.
  * @param {string} path Path to search.
- * @returns {Promise<any[]>} List of files in path.
+ * @returns {Promise<T[]>} List of files in path.
  */
-export const getContents = async (path: string): Promise<any[]> => {
+export const getContents = async <T>(path: string): Promise<T[]> => {
   const files = await glob(`${path}/**/*.{ts,js}`);
 
   const contents = await Promise.all(
