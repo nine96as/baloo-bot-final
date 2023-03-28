015745ce1fa27db3b3378ace91df0abe6d6c3387
996as <ashrafsaid79@gmail.com>
2023-03-28 01:08:18 +0000

    tweak: abstracted emojis

--- src/assets/emojis.ts
+++ src/assets/emojis.ts
@@ -1,12 +1,8 @@
 /**
- * Object containing various emojis used in the Discord bot.
+ * Object containing various emojis used by the Discord bot.
  */
 export const emojis = {
   category: '<:category:1025367604384239679>',
-
-  success: '<:success:1025364274610196582>',
-  error: '<:error:1025364273020547092>',
-  info: '<:info:1025365189434019933>',
 
   join: '<:join:1025373360785465374>',
   leave: '<:leave:1025373359392964618>',
@@ -14,25 +10,43 @@ export const emojis = {
   translate: '<:translate:1025378106833379420>',
   ping: '🏓',
   dice: '🎲',
-  math: '<:math:1025365970191122514>',
+  math: '<:math:1025365970191122514>'
+};
 
-  pages: {
-    first: '<:first:1025373353885839401>',
-    next: '<:next:1025373355479662644>',
-    previous: '<:previous:1025373357518114836>',
-    last: '<:last:1025373352497524806>'
-  },
+/**
+ * Object containing logging emojis used by the Discord bot.
+ */
+export const loggerEmojis = {
+  success: '<:success:1025364274610196582>',
+  error: '<:error:1025364273020547092>',
+  info: '<:info:1025365189434019933>'
+};
 
-  switch: {
-    on: '<:on:1025378108116848670>',
-    off: '<:off:1025378109756809256>'
-  },
+/**
+ * Object containing toggle emojis used by the Discord bot.
+ */
+export const toggleEmojis = {
+  on: '<:on:1025378108116848670>',
+  off: '<:off:1025378109756809256>'
+};
 
-  badge: {
-    owner: '<:owner:1025362154259492974>',
-    booster: '<:booster:1025374007341625344>',
-    staff: '<:staff:1025373363759222815>',
-    member: '<:member:1025367605919371286>',
-    bot: '<:bot:1025360506166448158>'
-  }
+/**
+ * Object containing pagination emojis used by the Discord bot.
+ */
+export const pageEmojis = {
+  first: '<:first:1025373353885839401>',
+  next: '<:next:1025373355479662644>',
+  previous: '<:previous:1025373357518114836>',
+  last: '<:last:1025373352497524806>'
+};
+
+/**
+ * Object containing badge emojis used by the Discord bot.
+ */
+export const badgeEmojis: { [key: string]: string } = {
+  owner: '<:owner:1025362154259492974>',
+  booster: '<:booster:1025374007341625344>',
+  staff: '<:staff:1025373363759222815>',
+  member: '<:member:1025367605919371286>',
+  bot: '<:bot:1025360506166448158>'
 };
