5ec8b364e02b49f4422a594e327ed78097e75989
996as <ashrafsaid79@gmail.com>
2023-03-28 00:05:18 +0000

    tweak: moved to #interfaces folder

---
+++ src/interfaces/command.ts
@@ -0,0 +1,51 @@
+import { Bot } from '#structures';
+import {
+  AutocompleteInteraction,
+  CommandInteraction,
+  ContextMenuCommandBuilder,
+  InteractionResponse,
+  Message,
+  MessageContextMenuCommandInteraction,
+  SlashCommandBuilder,
+  SlashCommandSubcommandsOnlyBuilder,
+  UserContextMenuCommandInteraction
+} from 'discord.js';
+
+/**
+ * Represents a command that can be executed by a user in a Discord server.
+ */
+export interface Command {
+  /**
+   * The data used to define the command, which could be one of several types depending on the type of command.
+   */
+  data:
+    | ContextMenuCommandBuilder
+    | SlashCommandBuilder
+    | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>
+    | SlashCommandSubcommandsOnlyBuilder;
+
+  /**
+   * Executes the logic for the command when it is invoked by a user.
+   * @param interaction - The interaction object representing the command invocation event.
+   * @param client - The Discord bot client instance that received the event.
+   * @returns A promise that resolves to a `InteractionResponse<boolean>`, `Message<boolean>`, or void.
+   */
+  execute(
+    interaction:
+      | MessageContextMenuCommandInteraction
+      | UserContextMenuCommandInteraction
+      | CommandInteraction,
+    client: Bot
+  ): Promise<InteractionResponse<boolean> | Message<boolean> | void>;
+
+  /**
+   * Executes the logic for the command when a user is selecting an autocomplete option.
+   * @param interaction - The interaction object representing the autocomplete event.
+   * @param client - The Discord bot client instance that received the event.
+   * @returns A promise that resolves to void.
+   */
+  autocomplete?(
+    interaction: AutocompleteInteraction,
+    client: Bot
+  ): Promise<void>;
+}
