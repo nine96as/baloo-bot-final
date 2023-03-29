f2dac6d1ae3e7f0b44592ca0f316b6e05a17ff3a
996as <nine96@skiff.com>
2023-03-29 00:51:07 +0000

    feat: added prisma const

---
+++ src/utils/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient({
+  errorFormat: 'pretty',
+  log: [
+    { level: 'warn', emit: 'event' },
+    { level: 'info', emit: 'event' },
+    { level: 'error', emit: 'event' }
+  ]
+});
