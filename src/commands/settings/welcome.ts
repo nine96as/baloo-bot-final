62420019ecb5174c8d85ff55ea29df253cd5a508
996as <nine96@skiff.com>
2023-03-29 00:46:04 +0000

    feat: implemented welcome system set command

---
+++ src/commands/settings/welcome.ts
@@ -0,0 +1,56 @@
+import {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  ChannelType,
+  PermissionFlagsBits,
+  TextBasedChannel
+} from 'discord.js';
+import { Command, SuccessEmbed } from '#interfaces';
+import { prisma } from '#utils';
+
+export const command: Command = {
+  data: new SlashCommandBuilder()
+    .setName('welcome')
+    .setDescription('⚙️ welcome system setup')
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('set')
+        .setDescription('⚙️ set the welcome channel for the guild')
+        .addChannelOption((option) =>
+          option
+            .setName('channel')
+            .setDescription('target channel')
+            .setRequired(true)
+            .addChannelTypes(ChannelType.GuildText)
+        )
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    if (interaction.options.getSubcommand() === 'set') {
+      if (interaction.inCachedGuild()) {
+        const { options } = interaction;
+
+        const channel = options.getChannel('channel') as TextBasedChannel;
+
+        await prisma.channel.upsert({
+          where: { channelId: channel.id },
+          create: { channelId: channel.id, guildId: interaction.guildId },
+          update: { channelId: channel.id }
+        });
+
+        await prisma.welcomeSystem.upsert({
+          where: { channelId: channel.id },
+          create: { guildId: interaction.guildId, channelId: channel.id },
+          update: { guildId: interaction.guildId, channelId: channel.id }
+        });
+
+        return interaction.reply({
+          embeds: [
+            new SuccessEmbed(`***set welcome channel as <#${channel.id}>***`)
+          ]
+        });
+      }
+    }
+  }
+};
