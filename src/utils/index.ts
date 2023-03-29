723201f166d416fb5d2142db3595ede288a440e5
996as <ashrafsaid79@gmail.com>
2023-03-29 00:50:21 +0000

    tweak: added prisma refs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './logger.js';
 export * from './config.js';
+export * from './prisma.js';
 export * from './getContents.js';
 export * from './generateWelcomeBanner.js';
 export * from './getRandomInt.js';
