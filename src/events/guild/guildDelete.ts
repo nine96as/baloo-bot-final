b0e4815fbd88c34dd54ed192ef065ddab80cb566
996as <ashrafsaid79@gmail.com>
2023-03-29 00:47:22 +0000

    feat: created guildDelete event

---
+++ src/events/guild/guildDelete.ts
@@ -0,0 +1,26 @@
+import { Event } from '#interfaces';
+import { Bot } from '#structures';
+import { logger, prisma } from '#utils';
+import { Events, Guild } from 'discord.js';
+
+export const event: Event = {
+  name: Events.GuildDelete,
+  execute: async (client: Bot, guild: Guild) => {
+    if (!guild || !guild.id) return;
+
+    try {
+      await prisma.guild.delete({ where: { guildId: guild.id } }).then(() => {
+        logger.info(
+          `left ${guild.name} (${guild.id}), and successfully deleted the records in the database.`,
+          { label: 'event' }
+        );
+      });
+    } catch (e) {
+      //   logger.error(
+      //     `joined ${guild.name} (${guild.id}), errored while deleting a record in the database.`,
+      //     { label: 'event' }
+      //   );
+      logger.error(e);
+    }
+  }
+};
