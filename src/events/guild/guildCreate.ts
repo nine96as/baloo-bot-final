5a6156635ea87b47227cff7d1a38c07de93be14b
996as <ashrafsaid79@gmail.com>
2023-03-29 00:47:13 +0000

    feat: created guildCreate event

---
+++ src/events/guild/guildCreate.ts
@@ -0,0 +1,32 @@
+import { Event } from '#interfaces';
+import { Bot } from '#structures';
+import { logger, prisma } from '#utils';
+import { Events, Guild } from 'discord.js';
+
+export const event: Event = {
+  name: Events.GuildCreate,
+  execute: async (client: Bot, guild: Guild) => {
+    if (!guild || !guild.id) return;
+
+    try {
+      await prisma.guild
+        .upsert({
+          where: { guildId: guild.id },
+          create: { guildId: guild.id },
+          update: { guildId: guild.id }
+        })
+        .then(() => {
+          logger.info(
+            `joined ${guild.name} (${guild.id}), and successfully created a record in the database.`,
+            { label: 'event' }
+          );
+        });
+    } catch (e) {
+      //   logger.error(
+      //     `Joined ${guild.name} (${guild.id}), errored while creating a record in the database.`,
+      //     { label: 'event' }
+      //   );
+      logger.error(e);
+    }
+  }
+};
