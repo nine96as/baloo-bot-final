d6a37d17fc46ffe251c728ddf3a244aa23f6b224
996as <nine96@skiff.com>
2023-03-29 00:49:03 +0000

    feat: created guildMemberAdd event

---
+++ src/events/guild/guildMemberAdd.ts
@@ -0,0 +1,34 @@
+import { Events, GuildMember, TextBasedChannel } from 'discord.js';
+import { Bot } from '#structures';
+import { Event } from '#interfaces';
+import { generateWelcomeBanner, prisma } from '#utils';
+
+export const event: Event = {
+  name: Events.GuildMemberAdd,
+  async execute(client: Bot, member: GuildMember) {
+    const { guild } = member;
+
+    const data = await prisma.welcomeSystem.findUnique({
+      where: { guildId: guild.id }
+    });
+
+    if (!data) return;
+
+    if (data.channelId) {
+      const channel = guild.channels.cache.get(
+        data.channelId
+      ) as TextBasedChannel;
+
+      // If there is no welcome channel, nothing to be done
+      if (!channel) return;
+
+      // Otherwise generate and send welcome banner
+      const img = await generateWelcomeBanner(member);
+
+      channel.send({
+        content: `<@${member.id}> welcome to **${guild.name}**`,
+        files: [img]
+      });
+    }
+  }
+};
