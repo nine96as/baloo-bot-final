f295861a24017f78cf9c09f06c4105ee62202c7a
996as <nine96@skiff.com>
2023-03-31 00:33:59 +0000

    tweak: added checkLockdownChannels ref

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ export * from './logger.js';
 export * from './config.js';
 export * from './prisma.js';
 export * from './getContents.js';
+export * from './checkLockdownChannels.js';
 export * from './generateWelcomeBanner.js';
 export * from './getRandomInt.js';
