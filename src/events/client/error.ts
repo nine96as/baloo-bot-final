745061e23330ab1e77cbc4e04681f693ea822d11
996as <nine96@skiff.com>
2023-04-02 17:22:20 +0000

    tweak: syntax improvements

--- src/events/client/error.ts
+++ src/events/client/error.ts
@@ -3,10 +3,10 @@ import { Bot } from '#structures';
 import { Event } from '#interfaces';
 import { logger } from '#utils';
 
-export const event: Event = {
+export const event = {
   name: Events.Error,
   once: true,
-  execute(_client: Bot, m: Message) {
+  execute: (_client: Bot, m: Message) => {
     logger.error(m);
   }
-};
+} satisfies Event;
