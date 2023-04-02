70c20403ceb213f1d23897e05c9983d65953f0c9
996as <nine96@skiff.com>
2023-04-02 17:30:36 +0000

    tweak: commenting

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
+/**
+ * Creates prisma client instance with error formatting layer
+ */
 export const prisma = new PrismaClient({
   errorFormat: 'minimal',
   log: [
