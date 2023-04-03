c9f94f9a5c9402acfdf084b894cea58c799ce253
996as <nine96@skiff.com>
2023-04-03 23:23:33 +0000

    tweak: sendLogMessages naming

--- src/events/guild/guildBanAdd.ts
+++ src/events/guild/guildBanAdd.ts
@@ -1,14 +1,14 @@
 import { Events, GuildBan } from 'discord.js';
 import { Bot } from '#structures';
 import { Event, InfoEmbed } from '#interfaces';
-import { sendLogMessages } from '#utils';
+import { sendLogMessage } from '#utils';
 
 export const event = {
   name: Events.GuildBanAdd,
   execute: (_client: Bot, ban: GuildBan) => {
     const { user, guild, reason } = ban;
 
-    return sendLogMessages(
+    return sendLogMessage(
       guild,
       new InfoEmbed(`***user ${user} banned.*\n
       reason:** \`${reason ?? 'Not specified'}\``)
