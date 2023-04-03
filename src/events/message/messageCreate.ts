2d162ab17628128812185f841e851faac3698201
996as <ashrafsaid79@gmail.com>
2023-04-03 23:25:08 +0000

    tweak: sendLogMessages naming, added SendAfkReturnMessage refs

--- src/events/message/messageCreate.ts
+++ src/events/message/messageCreate.ts
@@ -1,7 +1,7 @@
 import { Message, Events } from 'discord.js';
 import { Bot } from '#structures';
 import { EmojiEmbed, Event } from '#interfaces';
-import { logger, prisma } from '#utils';
+import { sendAfkReturnMessage, sendAfkMentionMessage } from '#utils';
 import { emojis } from '#assets';
 
 export const event = {
@@ -9,47 +9,14 @@ export const event = {
   execute: async (_client: Bot, message: Message) => {
     if (message.author.bot) return;
 
-    if (
-      await prisma.afkSystem.findUnique({
-        where: { userId: message.author.id }
-      })
-    ) {
-      try {
-        message.channel.send({
-          embeds: [
-            new EmojiEmbed(
-              emojis.return,
-              `***${message.author}: welcome back, your AFK has been removed.***`
-            )
-          ]
-        });
-        await prisma.afkSystem.delete({ where: { userId: message.author.id } });
-      } catch (e) {
-        logger.error(e);
-      }
-    }
+    sendAfkReturnMessage(
+      message,
+      new EmojiEmbed(
+        emojis.return,
+        `***${message.author}: welcome back, your AFK has been removed.***`
+      )
+    );
 
-    if (message.mentions.members?.size) {
-      message.mentions.members.forEach(async (m) => {
-        const data = await prisma.afkSystem.findUnique({
-          where: { userId: m.id }
-        });
-
-        if (!data) return;
-
-        if (data.userId) {
-          message.reply({
-            embeds: [
-              new EmojiEmbed(
-                emojis.afk,
-                `***${m} is AFK: \`${data.status}\` - <t:${Math.floor(
-                  data.createdAt.getTime() / 1000
-                )}:R>***`
-              )
-            ]
-          });
-        }
-      });
-    }
+    return sendAfkMentionMessage(message);
   }
 } satisfies Event;
