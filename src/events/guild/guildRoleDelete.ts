b2561ab876fba5fe850b7d27370d7adb8dec8052
996as <nine96@skiff.com>
2023-04-03 23:24:49 +0000

    tweak: sendLogMessages naming

--- src/events/guild/guildRoleDelete.ts
+++ src/events/guild/guildRoleDelete.ts
@@ -1,14 +1,14 @@
 import { Events, Role } from 'discord.js';
 import { Bot } from '#structures';
 import { Event, InfoEmbed } from '#interfaces';
-import { sendLogMessages } from '#utils';
+import { sendLogMessage } from '#utils';
 
 export const event = {
   name: Events.GuildRoleDelete,
   execute: (_client: Bot, role: Role) => {
     const { guild, name } = role;
 
-    return sendLogMessages(
+    return sendLogMessage(
       guild,
       new InfoEmbed(`***role*** \`${name}\` ***deleted.***`)
     );
