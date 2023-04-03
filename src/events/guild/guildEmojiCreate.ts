7ff587a0c9b021fe902f3c9df45c09c61621ce5c
996as <ashrafsaid79@gmail.com>
2023-04-03 22:19:51 +0000

    feat: implemented guildEmojiCreate event w logging

---
+++ src/events/guild/guildEmojiCreate.ts
@@ -0,0 +1,18 @@
+import { Events, GuildEmoji } from 'discord.js';
+import { Bot } from '#structures';
+import { Event, InfoEmbed } from '#interfaces';
+import { sendLogMessages } from '#utils';
+
+export const event = {
+  name: Events.GuildEmojiCreate,
+  execute: async (_client: Bot, emoji: GuildEmoji) => {
+    const { guild } = emoji;
+
+    const creator = await emoji.fetchAuthor();
+
+    return sendLogMessages(
+      guild,
+      new InfoEmbed(`***emoji ${emoji} created by ${creator}***`)
+    );
+  }
+} satisfies Event;
