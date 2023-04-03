18be354780f9c0b62da2a85f5190c5886dd9c6f0
996as <ashrafsaid79@gmail.com>
2023-04-03 23:24:49 +0000

    tweak: sendLogMessages naming

--- src/events/guild/guildRoleCreate.ts
+++ src/events/guild/guildRoleCreate.ts
@@ -1,14 +1,14 @@
 import { Events, Role } from 'discord.js';
 import { Bot } from '#structures';
 import { Event, InfoEmbed } from '#interfaces';
-import { sendLogMessages } from '#utils';
+import { sendLogMessage } from '#utils';
 
 export const event = {
   name: Events.GuildRoleCreate,
   execute: (_client: Bot, role: Role) => {
     const { permissions, guild } = role;
 
-    return sendLogMessages(
+    return sendLogMessage(
       guild,
       new InfoEmbed(`***role ${role} created*\n
       permissions:** \`${
