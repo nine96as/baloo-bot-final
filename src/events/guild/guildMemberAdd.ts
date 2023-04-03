5503673fa29d637f863faef47a4d907f8894f54d
996as <ashrafsaid79@gmail.com>
2023-04-03 23:24:23 +0000

    tweak: sendLogMessages naming, added SendWelcomeMessage refs

--- src/events/guild/guildMemberAdd.ts
+++ src/events/guild/guildMemberAdd.ts
@@ -1,7 +1,7 @@
-import { Events, GuildMember, TextBasedChannel } from 'discord.js';
+import { Events, GuildMember } from 'discord.js';
 import { Bot } from '#structures';
 import { EmojiEmbed, Event, InfoEmbed } from '#interfaces';
-import { generateWelcomeBanner, prisma, sendLogMessages } from '#utils';
+import { sendLogMessage, sendWelcomeMessage } from '#utils';
 import { emojis } from '#assets';
 
 export const event = {
@@ -9,33 +9,13 @@ export const event = {
   execute: async (_client: Bot, member: GuildMember) => {
     const { guild } = member;
 
-    // Checks if guild has a WelcomeSystem entry
-    const data = await prisma.welcomeSystem.findUnique({
-      where: { guildId: guild.id }
-    });
+    sendWelcomeMessage(
+      guild,
+      member,
+      new EmojiEmbed(emojis.join, `${member} welcome to **${guild.name}**`)
+    );
 
-    if (!data) return;
-
-    if (data.channelId) {
-      const channel = guild.channels.cache.get(
-        data.channelId
-      ) as TextBasedChannel;
-
-      // If there is no welcome channel, nothing to be done
-      if (!channel) return;
-
-      // Otherwise generate and send welcome banner
-      const img = await generateWelcomeBanner(member);
-
-      channel.send({
-        embeds: [
-          new EmojiEmbed(emojis.join, `${member} welcome to **${guild.name}**`)
-        ],
-        files: [img]
-      });
-    }
-
-    return sendLogMessages(
+    return sendLogMessage(
       guild,
       new InfoEmbed(`***member ${member} joined the server.***`)
     );
