0ef1c45dab5bcd7d088a520606e2ccbcc737f0ef
996as <nine96@skiff.com>
2023-04-03 23:23:13 +0000

    tweak: sendLogMessages naming

--- src/events/channel/channelDelete.ts
+++ src/events/channel/channelDelete.ts
@@ -1,14 +1,14 @@
 import { Events, GuildBasedChannel } from 'discord.js';
 import { Bot } from '#structures';
 import { Event, InfoEmbed } from '#interfaces';
-import { sendLogMessages } from '#utils';
+import { sendLogMessage } from '#utils';
 
 export const event = {
   name: Events.ChannelDelete,
   execute: (_client: Bot, channel: GuildBasedChannel) => {
     const { guild, name } = channel;
 
-    return sendLogMessages(
+    return sendLogMessage(
       guild,
       new InfoEmbed(`***channel*** \`${name}\` ***deleted.***`)
     );
