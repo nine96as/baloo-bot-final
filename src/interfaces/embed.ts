2985b2ba41655a460a58f748f5f3faf82e24824a
996as <nine96@skiff.com>
2023-04-04 00:22:09 +0000

    tweak: commenting

--- src/interfaces/embed.ts
+++ src/interfaces/embed.ts
@@ -1,6 +1,9 @@
 import { EmbedBuilder, EmbedData } from 'discord.js';
 import { loggerEmojis } from '#assets';
 
+/**
+ * Custom Embed class that extends EmbedBuilder
+ */
 export class Embed extends EmbedBuilder {
   constructor(data?: EmbedData) {
     if (!data) data = { color: 0x2f3136 };
@@ -8,6 +11,7 @@ export class Embed extends EmbedBuilder {
     super(data);
   }
 
+  // Custom method to add a single field to the embed
   addField(name: string, value: string, inline?: boolean): this {
     this.addFields([
       {
@@ -20,38 +24,62 @@ export class Embed extends EmbedBuilder {
   }
 }
 
+/**
+ * Custom EmojiEmbed class that extends the custom Embed class
+ */
 export class EmojiEmbed extends Embed {
   constructor(emoji: string, content: string) {
+    // Sets the embed's description to include the given emoji and content
     super({
       description: `${emoji} ${content}`
     });
   }
 }
 
+/**
+ * Custom SuccessEmbed class that extends the custom EmojiEmbed class
+ */
 export class SuccessEmbed extends EmojiEmbed {
   constructor(content: string) {
+    // Calls the constructor of the parent class with the success emoji and content
     super(loggerEmojis.success, content);
+    // Sets the embed color to green
     this.setColor('#00ff80');
   }
 }
 
+/**
+ * Custom WarnEmbed class that extends the custom EmojiEmbed class
+ */
 export class WarnEmbed extends EmojiEmbed {
   constructor(content: string) {
+    // Calls the constructor of the parent class with the warning emoji and content
     super(loggerEmojis.warn, content);
+    // Sets the embed color to yellow
     this.setColor('#ffff80');
   }
 }
 
+/**
+ * Custom ErrorEmbed class that extends the custom EmojiEmbed class
+ */
 export class ErrorEmbed extends EmojiEmbed {
   constructor(content: string) {
+    // Calls the constructor of the parent class with the error emoji and content
     super(loggerEmojis.error, content);
+    // Sets the embed color to red
     this.setColor('#ff8080');
   }
 }
 
+/**
+ * Custom InfoEmbed class that extends the custom EmojiEmbed class
+ */
 export class InfoEmbed extends EmojiEmbed {
   constructor(content: string) {
+    // Calls the constructor of the parent class with the info emoji and content
     super(loggerEmojis.info, content);
+    // Sets the embed color to orange
     this.setColor('#ff8040');
   }
 }
