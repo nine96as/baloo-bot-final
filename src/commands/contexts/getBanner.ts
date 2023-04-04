ce04d204e5f61b93f79a3255417831117ad9aa12
996as <ashrafsaid79@gmail.com>
2023-04-04 00:21:18 +0000

    feat: implemented getBanner command

---
+++ src/commands/contexts/getBanner.ts
@@ -0,0 +1,37 @@
+import {
+  ApplicationCommandType,
+  UserContextMenuCommandInteraction,
+  ContextMenuCommandBuilder
+} from 'discord.js';
+import { Command, Embed, WarnEmbed } from '#interfaces';
+
+export const command = {
+  data: new ContextMenuCommandBuilder()
+    .setName('getBanner')
+    .setType(ApplicationCommandType.User),
+  execute: async (interaction: UserContextMenuCommandInteraction) => {
+    if (interaction.inCachedGuild()) {
+      const { targetMember } = interaction;
+      const member = targetMember;
+
+      await member.user.fetch(true);
+
+      member.user.bannerURL()
+        ? interaction.reply({
+            embeds: [
+              new Embed()
+                .setColor('Random')
+                .setAuthor({
+                  iconURL: member.user.displayAvatarURL(),
+                  name: member.user.tag
+                })
+                .setImage(member.user.bannerURL({ size: 4096 }) || null)
+            ]
+          })
+        : interaction.reply({
+            embeds: [new WarnEmbed('***This user does not have a banner.***')],
+            ephemeral: true
+          });
+    }
+  }
+} satisfies Command;
