2c2bfad3d115668c14f94ab6be1b2de93e03a898
996as <ashrafsaid79@gmail.com>
2023-04-07 21:48:32 +0000

    tweak: reordered exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
-export * from './logger.js';
 export * from './config.js';
+export * from './logger.js';
 export * from './prisma.js';
 export * from './getContents.js';
 export * from './lockdownSystem/checkLockdownChannels.js';
